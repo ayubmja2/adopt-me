af1fdfc5764546c597aa5b81c543d6934f7979e8
Ayub <ayub@localhost.localdomain>
2024-01-01 17:35:10 -0500

    working on searchparams

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Pet from "./Pet";
 import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Nemo" animal="Cat" breed="Mixed" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Dog" animal="Doink" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
